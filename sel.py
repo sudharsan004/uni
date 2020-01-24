f7cb1365acf90b19d769d3d3e94bd7ce66243779
sudharsan <46211435+sudharsan004@users.noreply.github.com>
2020-01-24 13:15:16 +0530

    Update sel.py

--- sel.py
+++ sel.py
@@ -3,6 +3,7 @@ import sys,os
 import time
 import pandas as pd
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 #choose the correct chrome driver for your chrome version
 chromedriver = r"chromedriver"
@@ -17,20 +18,25 @@ browser.find_element_by_xpath('/html/body/div/div/div[2]/div/div[4]/div/div[2]/i
 df = pd.DataFrame({'token','Exchange-rate','Pool-size'})
 
 
+
+
 for token in range(2,200):
-    t=str(token)
-    browser.find_element_by_xpath('/html/body/div/div/div[2]/div/div[6]/div/div[2]/button/span').click()
-    browser.find_element_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div/div[3]/div['+t+']').click()
-    time.sleep(1.5)
-    exchange_rate=browser.find_element_by_xpath('/html/body/div/div/div[2]/div/div[7]/div[2]/div[1]/span[2]').text
-    print(exchange_rate)
-    lst=exchange_rate.split(' ')
-    token_name=lst[-1]
-    print(token_name)
-    current_pool_size=browser.find_element_by_xpath('/html/body/div/div/div[2]/div/div[7]/div[2]/div[2]/span[2]').text
-    print('pool-size'+current_pool_size)
-    i=token-2
-    df.loc[i]={token_name,exchange_rate,current_pool_size}
+    try:
+        t=str(token)
+        browser.find_element_by_xpath('/html/body/div/div/div[2]/div/div[6]/div/div[2]/button/span').click()
+        browser.find_element_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div/div[3]/div['+t+']').click()
+        time.sleep(1.5)
+        exchange_rate=browser.find_element_by_xpath('/html/body/div/div/div[2]/div/div[7]/div[2]/div[1]/span[2]').text
+        print(exchange_rate)
+        lst=exchange_rate.split(' ')
+        token_name=lst[-1]
+        print(token_name)
+        current_pool_size=browser.find_element_by_xpath('/html/body/div/div/div[2]/div/div[7]/div[2]/div[2]/span[2]').text
+        print('pool-size'+current_pool_size)
+        i=token-2
+        df.loc[i]={token_name,exchange_rate,current_pool_size}
+    except nosuchelementexception:
+       pass
 print(df)
     
 
